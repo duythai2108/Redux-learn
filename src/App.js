198eee56aa6511b6d51815fde57c2877c1518397
duythai2108 <thaiduy21082000@gmail.com>
2023-08-05 15:43:07 +0700

    useDarkMode redux

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Counter from "./components/Counter";
 import { useDispatch, useSelector } from "react-redux";
 import Card from "./components/Card";
 import { toggleDarkMode } from "./redux-toolkit/globalSlice";
+import useDarkMode from "./hooks/useDarkMode";
 
 function App() {
-  // const count = useSelector((state) => state.counter.count);
-  const globalOptions = useSelector((state) => state.global);
-  // console.log("🚀 ~ App ~ globalOptions:", globalOptions);
-  // const dispatch = useDispatch();
-  // const handleToggleDarkMode = () => {
-  //   dispatch(toggleDarkMode(true));
-  // };
   return (
     <div>
-      <Card></Card>
-      {/* <button onClick={handleToggleDarkMode}>Toggle dark mode</button> */}
+      <Counter></Counter>
     </div>
   );
 }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,27 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-// import { decrement, increment, incrementbyvalue } from "../redux/counter";
+import useDarkMode from "../hooks/useDarkMode";
 import {
-  increment,
   decrement,
+  increment,
   incrementbyvalue,
 } from "../redux-toolkit/counterSlice";
+import { toggleDarkMode } from "../redux-toolkit/globalSlice";
+import Card from "./Card";
+// import { decrement, increment, incrementByValue } from "../redux/counter";
+
 const Counter = () => {
   const count = useSelector((state) => state.counter.count);
   const dispatch = useDispatch();
   const handleIncrement = () => {
     dispatch(increment());
   };
+  // // Write new function name handleDecrement
   const handleDecrement = () => {
     dispatch(decrement());
   };
+  // // handleIncrementByValue
   const handleIncrementByValue = () => {
     dispatch(incrementbyvalue({ value: 10 }));
   };
+
   return (
-    <div className="flex flex-col items-center p-5 bg-white shadow w-[200px] mx-auto mt-10">
+    <div className="flex flex-col items-center p-5 bg-white shadow w-[400px] mx-auto mt-10">
       <h2 className="mb-5">Count: {count}</h2>
-      <div className="flex justify-center items-center gap-x-5">
+      <div className="flex items-center justify-center gap-x-5">
         <button
           className="inline-block p-2 border border-gray-200"
           onClick={handleIncrement}
@@ -40,9 +47,37 @@ const Counter = () => {
         >
           Increase by 10
         </button>
+        {/*
+         */}
+      </div>
+      <div className="mt-10">
+        <SwitchDarkMode></SwitchDarkMode>
+      </div>
+      <div className="mt-5">
+        <Card></Card>
       </div>
     </div>
   );
 };
+function SwitchDarkMode() {
+  const [darkMode, setDarkMode] = useDarkMode();
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(toggleDarkMode(darkMode));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  const handleToggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    dispatch(toggleDarkMode(!darkMode));
+  };
+  return (
+    <button
+      className="p-5 border border-gray-300 bg-green-400 text-white text-sm rounded-lg"
+      onClick={handleToggleDarkMode}
+    >
+      Toggle dark mode
+    </button>
+  );
+}
 export default Counter;
